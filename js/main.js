7ab9dc3bd91f6e250c7842629d0fc7e13b9588a4
Anna_Vasilchenko <motolive2009@rambler.ru>
2022-09-20 14:27:59 +0300

    Реализует механизм слайдера в блоке team

--- js/main.js
+++ js/main.js
@@ -60,3 +60,62 @@ function onTabServClick(item) {
 };
 
 document.querySelector('.services-paginator__button').click();
+
+// slider team
+
+const teamSlides = document.querySelectorAll('.employers__item');
+const teamPaginators = document.querySelectorAll('.team-paginator__item');
+// активный слайд в который будут записываться изменения
+let teamActiveSlideIndex = 0;
+
+// Вешают на элементы классы
+function teamActiveSlide (n) {
+    for(slide of teamSlides) {
+        slide.classList.remove('active-slide-team');
+    }
+
+    teamSlides[n].classList.add('active-slide-team');
+}
+function teamActivePaginator(n) {
+    for(dot of teamPaginators) {
+        dot.classList.remove('active-paginator');
+    }
+    teamPaginators[n].classList.add('active-paginator');
+}
+
+// Объединение двух функций с навешиванием классов
+function teamCurrentSlide(item) {
+    teamActiveSlide(item);
+    teamActivePaginator(item);
+}
+// Бесконечный механизм перелистывания слайдов
+function teamNextSlide () {
+    if (teamActiveSlideIndex == teamSlides.length-1) {
+        teamActiveSlideIndex = 0;
+        teamCurrentSlide(teamActiveSlideIndex);
+    } else {
+        teamActiveSlideIndex ++;
+        teamCurrentSlide(teamActiveSlideIndex);
+    }
+
+}
+function teamPrevSlide () {
+    if (teamActiveSlideIndex == 0) {
+        teamActiveSlideIndex = teamSlides.length-1;
+        teamCurrentSlide(teamActiveSlideIndex);
+    } else {
+        teamActiveSlideIndex --;
+        teamCurrentSlide(teamActiveSlideIndex);
+    }
+}
+
+// Механизм перелистывания слайдов путем клика на точку
+teamPaginators.forEach(function(item, indexDot) {
+    item.addEventListener('click', function(){
+        teamActiveSlideIndex = indexDot;
+        teamCurrentSlide(teamActiveSlideIndex);
+    })
+})
+
+// Механизм слайдшоу
+setInterval(teamNextSlide, 5000);
